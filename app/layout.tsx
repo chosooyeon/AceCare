2ae2e4c086f907707c76e9cfa50a1c2f6847f9dd
kihwan.lee <kihwan.lee@lemonhc.com>
2022-11-28 11:07:33 +0900

    init

---
+++ app/layout.tsx
@@ -0,0 +1,27 @@
+import './globals.css'
+// import Footer from '../components/common/Footer'
+import Header from '../components/common/Header'
+// import SideBar from '../components/common/SideBar'
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      {/*
+        <head /> will contain the components returned by the nearest parent
+        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
+      */}
+      <head />
+      <body className='bg-white'>
+        <Header />
+        {/* <SideBar /> */}
+        <div className='text-black'>/laytout.tsx</div>
+        {children}
+        {/* <Footer /> */}
+      </body>
+    </html>
+  )
+}
---
+++ app/page.tsx
@@ -0,0 +1,17 @@
+import Image from 'next/image'
+import styles from './page.module.css'
+import { Suspense } from "react";
+// import Header from '../components/common/Header'
+
+export default function Home() {
+  return (
+    <>
+      {/* <Suspense fallback={<div>Loading...</div>}>
+          <Header/>
+      </Suspense> */}
+      <section className=''>
+        <div className='text-black'>/page.tsx ,Root Page</div>
+      </section>
+    </>
+  )
+}

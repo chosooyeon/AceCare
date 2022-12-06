a2a6f21275257e49f0959ff74a0ca623b213dc9c
sooyeon.cho <sooyeon.cho@lemonhc.com>
2022-12-06 18:33:26 +0900

    :hammer:[MODI]goods.ts 수정

--- goodsData/coronaGoods.ts
+++ goodsData/coronaGoods.ts
@@ -1,14 +1,50 @@
 export const goodsList = [
     {
-        id: "1",
-        name: "손세정제",
-        category: "corona",
-        image_path: ""
+        id: "11",
+        name: "휴비딕 살균소독제",
+        category: "disinfectant",
+        eng_name:"HLNS",
+        image_path: "/img/disinfectant_HLNS.jpg",
+        img_detail_path: ""
     },
     {
-        id: "2",
-        name: "손세정제2",
-        category: "corona",
-        image_path: ""
+        id: "12",
+        name: "하우로제 살균소독제",
+        category: "disinfectant",
+        eng_name:"HOEROZE",
+        image_path: "/img/disinfectant_HLNS.jpg",
+        img_detail_path: ""
+    },
+    {
+        id: "21",
+        name: "애니가드 마스크",
+        category: "mask",
+        eng_name:"ANYGUARD",
+        image_path: "/img/mask_ANYGUARD.jpg",
+        img_detail_path: ""
+    },
+    {
+        id: "22",
+        name: "코마스 마스크",
+        category: "mask",
+        eng_name:"KOMAS",
+        image_path: "/img/mask_KOMAS.jpg",
+        img_detail_path: ""
+    },
+    {
+        id: "51",
+        name: "에니랩 코로나키트",
+        category: "coronaKit",
+        eng_name:"ANYLAB",
+        image_path: "/img/coronaKit_ANYLAB.jpg",
+        img_detail_path: ""
+    },
+    {
+        id: "61",
+        name: "휴비딕 온도계",
+        category: "thermometor",
+        eng_name:"KOMAS",
+        image_path: "/img/mask_KOMAS.jpg",
+        img_detail_path: ""
     }
 ]

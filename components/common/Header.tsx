912e7315166b6437d056cbe0474960b9b8241391
kihwan lee <victoryhwan7@gmail.com>
2022-12-08 01:04:38 +0900

    :hammer: 헤더 nav메뉴 카테고리별 상품 라우터 연결

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -1,10 +1,29 @@
 import Link from 'next/link'
 import React, { useState, useEffect } from "react";
+import DropDownMenu from '../normal/DropDownMenu';
+import { goodsList } from '../../goodsData/coronaGoods';
+
+async function getData() {
+  // let reqUrl = `${mainUrl}/api/user/1`
+  let reqUrl = `http://localhost:3000/api/user/1`
+  const res = await fetch(reqUrl, 
+    { 
+      // cache: 'no-store' //getServerSideProps(SSR) : 서버에서 api를 요청한 후에 렌더링을 한다.
+      cache: 'force-cache' //getStaticProps(SSG) : 빌드된 데이터를 서버에 json,html 파일을 가지고 있는다.
+      // next: { revalidate:10} //getStaticProps(ISR) : 시간 주기로 서버에서 렌더링을 한다.
+  });
+  return res.json();
+}
 
 export default function Header() {
   // const name = getData();
   // console.log(JSON.stringify(name))
   console.log('header log : 클라이언트단이 아닌 서버에 찍힘')
+
+  const disinfectantData = goodsList.filter((x)=>x.category === 'disinfectant') //todo: SSG로 변경 필요
+  const maskData = goodsList.filter((x)=>x.category === 'mask')                 //todo: SSG로 변경 필요
+  const coronaKitData = goodsList.filter((x)=>x.category === 'coronaKit')       //todo: SSG로 변경 필요
+  const thermometorData = goodsList.filter((x)=>x.category === 'thermometor')   //todo: SSG로 변경 필요
 
     return (
       <header className="sticky top-0 z-50 bg-yellow-100 laptop:z-50 w-full max-w-8xl mx-auto flex-none flex h-16 laptop:h-20 shadow-lg justify-center">
@@ -19,6 +38,10 @@ export default function Header() {
   
               <nav className="hidden tablet:block">
                 <ul className="inline-flex">
+                <li className='m-10 w-15 list-none text-black'><DropDownMenu buttonName='disinfectant' items={disinfectantData}/></li>
+                <li className='m-10 w-15 list-none text-black'><DropDownMenu buttonName='mask' items={maskData}/></li>
+                <li className='m-10 w-15 list-none text-black'><DropDownMenu buttonName='coronaKit' items={coronaKitData}/></li>
+                <li className='m-10 w-15 list-none text-black'><DropDownMenu buttonName='thermometor' items={thermometorData}/></li>
                     <li className='m-10 w-15 list-none text-black'><Link href="/">Root</Link></li>
                     {/* <li className='m-10 w-15 list-none text-black'><Link href="/playground">Playground</Link></li>
                     <li className='m-10 w-15 list-none text-black'><Link href="/about">about</Link></li>
---
+++ components/normal/DropDownMenu.tsx
@@ -0,0 +1,51 @@
+
+import React, { useState, useEffect } from "react";
+import Link from 'next/link';
+
+//todo: items type 귀찮아서 안했지만 api가 명확해지면 타입지정
+// export type Item = {
+//     text: string;
+//     slug?: string;
+//   };
+
+export default function DropDownMenu({buttonName, items}:{buttonName:string, items:any}) {
+
+  return (
+    <div className="group">
+      <button
+        type="button"
+        className="inline-flex w-full justify-center rounded-md px-4 py-2 text-sm font-medium"
+        id="menu-button"
+        aria-expanded="true"
+        aria-haspopup="true"
+      >
+        {buttonName}
+      </button>
+
+      <div
+        className={`hidden group-hover:block absolute bg-white w-56 origin-top-right rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none -mt-1`}
+        role="menu"
+        aria-orientation="vertical"
+        aria-labelledby="menu-button"
+        tabIndex={-1}
+      >
+        <div className="py-0" role="none">
+          {items.map((item:any, index:any) => {
+            return (
+              <Link
+                key={index}
+                href={`/goods/${item.category}/${item.id}`}
+                className="text-gray-700 block px-4 py-2 text-sm hover:bg-gray-300"
+                role="menuitem"
+                id="menu-item-0"
+                tabIndex={-1}
+              >
+                {item.name}
+              </Link>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+}

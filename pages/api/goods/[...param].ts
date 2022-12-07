74b57dbf116a74e8ceecb878efd99cf7919c6fb7
kihwan.lee <kihwan.lee@lemonhc.com>
2022-12-07 17:54:33 +0900

    상품조회 api 수정 (전체상품/카테고리 전체상품/상품단건) 조회 기능 적용

--- pages/api/goods/[...param].ts
+++ pages/api/goods/[...param].ts
@@ -1,40 +1,42 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextRequest } from 'next/server'
-import { goodsList } from '../../../goodsData/coronaGoods'
+import type { NextRequest } from "next/server";
+import { goodsList } from "../../../goodsData/coronaGoods";
 
 export const config = {
-    runtime: 'experimental-edge',
-  }
-  
-  export default async function handler(req: NextRequest) {
-    const query = req.nextUrl.pathname.split('/')
-    console.log(query)
+  runtime: "experimental-edge",
+};
 
-    if(query.length < 5){
-        return new Response(
-            JSON.stringify({msg:"url error"}),
-            {
-              status: 401,
-              headers: {
-                'content-type': 'application/json',
-              },
-            }
-          )
-    }else{
-        let category:string = query[3]
-        let goodsId:string = query[4]
-        let goodsInfo = goodsList.filter(x => x.id === goodsId  && x.category === category)
-        
-        return new Response(
-        JSON.stringify(goodsInfo),
-        {
-            status: 200,
-            headers: {
-            'content-type': 'application/json',
-            },
-        }
-        )
+export default async function handler(req: NextRequest) {
+  let returnObj = {};
+  let returnStatus = 200;
+
+  const query = req.nextUrl.pathname.split("/");
+  console.log(query);
+  if (query.length === 4) {
+    let category: string = query[3];
+    if (category === "all") {
+      //전체 상품조회
+      returnObj = goodsList;
+    } else {
+      //카테고리 전체 상품조회
+      returnObj = goodsList.filter((x) => x.category === category);
     }
-
-    
+  } else if (query.length === 5) {
+    //상품조회
+    let category: string = query[3];
+    let goodsId: string = query[4];
+    returnObj = goodsList.filter(
+      (x) => x.id === goodsId && x.category === category
+    );
+  } else {
+    returnObj = { msg: "url error" };
+    returnStatus = 401;
   }
+
+  return new Response(JSON.stringify(returnObj), {
+    status: returnStatus,
+    headers: {
+      "content-type": "application/json",
+    },
+  });
+}

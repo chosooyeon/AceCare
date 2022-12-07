6dd1289a2d3493b51170ee94d09f15dbcf529de0
kihwan.lee <kihwan.lee@lemonhc.com>
2022-12-07 19:23:21 +0900

    :bug: generateStaticParams key change

--- app/goods/[category]/[goodsId]/page.tsx
+++ app/goods/[category]/[goodsId]/page.tsx
@@ -1,35 +1,34 @@
-import { usePathname } from 'next/navigation';
-import Image from 'next/image';
-import { goodsList } from '../../../../goodsData/coronaGoods'
+import { usePathname } from "next/navigation";
+import Image from "next/image";
+import { goodsList } from "../../../../goodsData/coronaGoods";
 
 export async function generateStaticParams() {
-    // let category = 'disinfectant'
-    // let goodsId = '12'
-    // const revalidatedData = await fetch(`http://localhost:3000/api/goods/${category}/${goodsId}`, {
-    //     next: { revalidate: 10 },
-    // });
+  const rows = goodsList;
 
-    const rows = goodsList
-
-    return rows?.map((row:any) => ({
-        categorySlug: row.category,
-        productId: row.id,
-    }));
+  return rows?.map((row: any) => ({
+    category: row.category,
+    goodsId: row.id,
+  }));
 }
 
 export default function Goods() {
   const pathname = usePathname();
-  let category = pathname?.split("/")[2]
+  let category = pathname?.split("/")[2];
   return (
     <>
-      <section className=''>
-          {/*<h1 className='text-black'>path : {path}</h1>*/}
-          <h1>카테고리 &gt; {category}</h1>
-          <Image src="/img/disinfectant_HLNS.jpg" width={512} height={512} alt=""/>
+      <section className="">
+        {/*<h1 className='text-black'>path : {path}</h1>*/}
+        <h1>카테고리 &gt; {category}</h1>
+        <Image
+          src="/img/disinfectant_HLNS.jpg"
+          width={512}
+          height={512}
+          alt=""
+        />
 
-          <h1>상세정보</h1>
-          <div className='text-black'>이곳은 상품 상세 페이지 입니다.</div>
+        <h1>상세정보</h1>
+        <div className="text-black">이곳은 상품 상세 페이지 입니다.</div>
       </section>
     </>
-  )
+  );
 }

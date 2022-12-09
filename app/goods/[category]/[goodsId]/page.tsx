7f8ed9b09a4ed65d0aa800494a550566392b204d
sooyeon.cho <sooyeon.cho@lemonhc.com>
2022-12-09 17:35:58 +0900

    [MODI]일단 cros 오류부터 해결...

--- app/goods/[category]/[goodsId]/page.tsx
+++ app/goods/[category]/[goodsId]/page.tsx
@@ -5,7 +5,8 @@ import Image from "next/image";
 import { goodsList } from "../../../../goodsData/coronaGoods";
 
 async function getData() {
-  let reqUrl = `https://acecare.vercel.app/api/goods/all`
+  // let reqUrl = `https://acecare.vercel.app/api/goods/all`
+  let reqUrl = `http://localhost:3000/api/goods/all`
   const res = await fetch(reqUrl,
       {
         // cache: 'no-store' //getServerSideProps(SSR) : 서버에서 api를 요청한 후에 렌더링을 한다.
@@ -16,7 +17,7 @@ async function getData() {
 }
 
 export async function generateStaticParams() {
-  const rows = JSON.parse(await getData());
+  const rows = await getData();
   // const rows = goodsList;
 
   return rows?.map((row: any) => ({
@@ -26,12 +27,13 @@ export async function generateStaticParams() {
 }
 
 async function getGoodsData(category:any, goodsId:any) {
-  let reqUrl = `https://acecare.vercel.app/api/goods/${category}/${goodsId}`
+  // let reqUrl = `https://acecare.vercel.app/api/goods/${category}/${goodsId}`
+  let reqUrl = `http://localhost:3000/api/goods/${category}/${goodsId}`
   const res = await fetch(reqUrl,
       {
         // cache: 'no-store' //getServerSideProps(SSR) : 서버에서 api를 요청한 후에 렌더링을 한다.
-        // cache: 'force-cache' //getStaticProps(SSG) : 빌드된 데이터를 서버에 json,html 파일을 가지고 있는다.
-        next: { revalidate:10} //getStaticProps(ISR) : 시간 주기로 서버에서 렌더링을 한다.
+        cache: 'force-cache' //getStaticProps(SSG) : 빌드된 데이터를 서버에 json,html 파일을 가지고 있는다.
+        // next: { revalidate:10} //getStaticProps(ISR) : 시간 주기로 서버에서 렌더링을 한다.
       });
   return res.json();
 }

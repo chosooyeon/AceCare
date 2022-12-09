dcd99366b78349bfca7ef47aecdde8860456c100
sooyeon.cho <sooyeon.cho@lemonhc.com>
2022-12-09 15:40:48 +0900

    [ADD]SSG fetch ing

--- app/goods/[category]/[goodsId]/page.tsx
+++ app/goods/[category]/[goodsId]/page.tsx
@@ -1,9 +1,23 @@
+'use client'
+
 import { usePathname } from "next/navigation";
 import Image from "next/image";
 import { goodsList } from "../../../../goodsData/coronaGoods";
 
+async function getData() {
+  let reqUrl = `https://acecare.vercel.app/api/goods/all`
+  const res = await fetch(reqUrl,
+      {
+        // cache: 'no-store' //getServerSideProps(SSR) : 서버에서 api를 요청한 후에 렌더링을 한다.
+        cache: 'force-cache' //getStaticProps(SSG) : 빌드된 데이터를 서버에 json,html 파일을 가지고 있는다.
+        // next: { revalidate:10} //getStaticProps(ISR) : 시간 주기로 서버에서 렌더링을 한다.
+      });
+  return res.json();
+}
+
 export async function generateStaticParams() {
-  const rows = goodsList;
+  const rows = JSON.parse(await getData());
+  // const rows = goodsList;
 
   return rows?.map((row: any) => ({
     category: row.category,
@@ -11,16 +25,34 @@ export async function generateStaticParams() {
   }));
 }
 
-export default function Goods() {
+async function getGoodsData(category:any, goodsId:any) {
+  let reqUrl = `https://acecare.vercel.app/api/goods/${category}/${goodsId}`
+  const res = await fetch(reqUrl,
+      {
+        // cache: 'no-store' //getServerSideProps(SSR) : 서버에서 api를 요청한 후에 렌더링을 한다.
+        // cache: 'force-cache' //getStaticProps(SSG) : 빌드된 데이터를 서버에 json,html 파일을 가지고 있는다.
+        next: { revalidate:10} //getStaticProps(ISR) : 시간 주기로 서버에서 렌더링을 한다.
+      });
+  return res.json();
+}
+
+
+export default async function Goods() {
+
   const pathname = usePathname();
   let category = pathname?.split("/")[2];
+  let goodsId = pathname?.split("/")[3];
+
+  let res = await getGoodsData(category,goodsId)
+  console.log(res,"res")
+
   return (
     <>
       <section className="">
         {/*<h1 className='text-black'>path : {path}</h1>*/}
         <h1>카테고리 &gt; {category}</h1>
         <Image
-          src="/img/disinfectant_HLNS.jpg"
+          src={res.image_path}
           width={512}
           height={512}
           alt=""
--- pages/api/goods/[...param].ts
+++ pages/api/goods/[...param].ts
@@ -16,13 +16,16 @@ export default async function handler(req: NextRequest) {
     let category: string = query[3];
     if (category === "all") {
       //전체 상품조회
+      //https://acecare.vercel.app/api/goods/all
       returnObj = goodsList;
     } else {
       //카테고리 전체 상품조회
+      // https://acecare.vercel.app/api/goods/[categoryId]
       returnObj = goodsList.filter((x) => x.category === category);
     }
   } else if (query.length === 5) {
     //상품조회
+    // https://acecare.vercel.app/api/goods/[categoryId]/[goodsId]
     let category: string = query[3];
     let goodsId: string = query[4];
     returnObj = goodsList.filter(

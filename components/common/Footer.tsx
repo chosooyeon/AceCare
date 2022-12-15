24b5fdc3e84a838b23d0db7594725c3d121436ed
sooyeon.cho <sooyeon.cho@lemonhc.com>
2022-12-15 15:19:12 +0900

    :hammer:footer 추가

--- components/common/Footer.tsx
+++ components/common/Footer.tsx
@@ -1,6 +1,16 @@
 export default function Footer() {
     return(
         <>
+            <div className="bg-slate-100 py-8 mx-auto">
+                <div className="w-[1280px]">
+                    <b>ABOUT US</b>
+                    <div className="mt-4">
+                        <span>에이스케어</span><br/>
+                        <span>서울특별시 강서구 양천로 470, 그레이스힐 지하2층 222호 (등촌동)</span><br/>
+                        <span>대표 김순미</span>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }

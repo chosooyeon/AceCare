2c8f82645cf6d2318f36632f9176ceb55425373d
kihwan.lee <kihwan.lee@lemonhc.com>
2022-12-20 19:32:06 +0900

    :hammer: 상품 카테고리 api 수정

--- components/api/goods/index.ts
+++ components/api/goods/index.ts
@@ -1,7 +1,6 @@
 // 추후 mode 에 따라서 분기처리 "http://localhost:3000"
-import {headers} from "next/headers";
 
-let URL = "http://localhost:3000"
+let URL = "https://acecare.vercel.app"
 const SSR:any = { cache: "no-store", headers:{'Accept':'application/json'}}       //getServerSideProps(SSR) : 서버에서 api를 요청한 후에 렌더링을 한다.
 const SSG:any = { cache: "force-cache", headers:{'Accept':'application/json'}}    //getStaticProps(SSG) : 빌드된 데이터를 서버에 json,html 파일을 가지고 있는다.
 const ISR:any = { next: {revalidate: 10}, headers:{'Accept':'application/json'}}    //getStaticProps(ISR) : 시간 주기로 서버에서 렌더링을 한다.
@@ -12,18 +11,17 @@ async function getGoodsAll() {
     return res.json();
 }
 
-async function getGoodsData(category: any, goodsId: any) {
+async function getGoodsData(category: string, goodsId: number) {
     let reqUrl = `${URL}/api/goods/${category}/${goodsId}`;
     const res = await fetch(reqUrl, SSG);
     return res.json();
 }
 
-async function getGoodsCategoryData(category: any) {
+async function getGoodsCategoryData(category: string) {
     let reqUrl = `${URL}/api/goods/${category}`;
     const res = await fetch(reqUrl, SSG);
     return res.json();
 }
-
 
 async function getCategory(category: any){
     let reqUrl = `${URL}/api/category/${category}`;

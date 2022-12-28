7217844539a342fc01a198b8671b68058e520927
kihwan lee <victoryhwan7@gmail.com>
2022-12-29 02:40:53 +0900

    :zap: 메인 section fadein 애니메이션 적용

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
+'use client'
 import Card from '@components/goods/Card'
+import React, { useState, useEffect } from "react";
 
 export default function Home() {
   const sections = [
@@ -7,6 +9,27 @@ export default function Home() {
     {title: '코로나키트', category: 'coronaKit'},
     {title: '온도계', category: 'thermometor'}
   ]
+
+  useEffect(()=>{
+    const elements = document.querySelectorAll('section');
+        
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        let elem:any = entry.target;
+
+        if(entry.intersectionRatio > 0){
+          elem.classList.add('slide')
+        }else{
+          elem.classList.remove('slide')
+        }
+      });
+    });
+
+    elements.forEach((el) => {
+        observer.observe(el);
+    });
+
+  }, [])
   return (
     <div className='w-[1024px] container my-12 mx-auto px-4 md:px-12'>
       {

894ad601d4d914529ec421c99ec386d6dc01702f
sooyeon.cho <sooyeon.cho@lemonhc.com>
2022-12-29 14:24:41 +0900

    :hammer: custom Anim hook 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 import Card from '@components/goods/Card'
-import React, { useState, useEffect } from "react";
+import {SlideAnim} from '@components/hooks/Anim'
 
 export default function Home() {
   const sections = [
@@ -9,31 +9,9 @@ export default function Home() {
     {title: '코로나키트', category: 'coronaKit'},
     {title: '온도계', category: 'thermometor'}
   ]
-
-  useEffect(()=>{
-    const elements = document.querySelectorAll('.card');
-    
-    const observer = new IntersectionObserver((entries) => {
-      entries.forEach((entry) => {
-        let elem:any = entry.target;
-
-        if(entry.intersectionRatio > 0){
-          elem.classList.add('slide')
-        }
-        // else{
-        //   elem.classList.remove('slide')
-        // }
-      });
-    });
-
-    const debounce = setTimeout(()=>{
-      elements.forEach((el) => {
-          observer.observe(el);
-      });
-    },1000);
-
-    return () => clearTimeout(debounce)
-  }, [])
+  
+  SlideAnim()
+  
   return (
     <div className='w-[1024px] container my-12 mx-auto px-4 md:px-12'>
       {
---
+++ components/hooks/Anim.ts
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from "react";
+
+function SlideAnim(){
+  useEffect(()=>{
+    const elements = document.querySelectorAll('.card');
+    
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        let elem:any = entry.target;
+
+        if(entry.intersectionRatio > 0){
+          elem.classList.add('slide')
+        }
+        // else{
+        //   elem.classList.remove('slide')
+        // }
+      });
+    });
+
+    const debounce = setTimeout(()=>{
+      elements.forEach((el) => {
+          observer.observe(el);
+      });
+    },1000);
+
+    return () => clearTimeout(debounce)
+  }, [])
+}
+
+export {SlideAnim}
